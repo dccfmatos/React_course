6f28ef2494fbfd8f0cbc0f2d93ae72f9d34b3d64
Diana Ferreira Matos <47236784+dccfmatos@users.noreply.github.com>
2020-06-01 11:25:41 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { id: 'asfa1', name: 'Max', age: 28 },
-      { id: 'vasdf1', name: 'Manu', age: 29 },
-      { id: 'asdf11', name: 'Stephanie', age: 26 }
+      { id: '111', name: 'Max', age: 28 },
+      { id: '222', name: 'Manu', age: 29 },
+      { id: '333', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -21,32 +21,36 @@ class App extends Component {
     const person = {
       ...this.state.persons[personIndex]
     };
-
-    // const person = Object.assign({}, this.state.persons[personIndex]);
-
+    
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
+    
 
     this.setState( {persons: persons} );
   }
 
+
   deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons: persons});
-  }
+  } 
+
+
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState( { showPersons: !doesShow } );
-  }
+  } 
+  
+  
 
   render () {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -58,29 +62,50 @@ class App extends Component {
     if ( this.state.showPersons ) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name} 
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
-          })}
+        {this.state.persons.map((person, index) => {
+          return <Person 
+            click={() => this.deletePersonHandler(index)}
+            name={person.name}
+            age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, persons.id)}
+            />
+        })}
+         
         </div>
       );
+
+      style.backgroundColor = 'red';
     }
+
+    //variable classes is just a String joined with an empty space ('red bold')
+    //---->>let classes = ['red', 'bold'].join(' ');
+
+
+    // ------
+    // to use in paragraph in return()
+    //red and bold are the classes defined in .css
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); //classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); //classes = ['red', 'bold']
+    }
+
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
+        {/* classes is just an array, we need to pass a string: classes.join(' ')*/}
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  
   }
 }
 
