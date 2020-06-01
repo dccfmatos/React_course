2b0363add2946ab2cbd307589f8acd7912a0476f
Diana Ferreira Matos <47236784+dccfmatos@users.noreply.github.com>
2020-06-01 11:25:01 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: '111', name: 'Max', age: 28 },
+      { id: '222', name: 'Manu', age: 29 },
+      { id: '333', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    
+
+    this.setState( {persons: persons} );
+  }
+
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  } 
+
+
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
+  } 
+  
+  
+
+  render () {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+        {this.state.persons.map((person, index) => {
+          return <Person 
+            click={() => this.deletePersonHandler(index)}
+            name={person.name}
+            age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, persons.id)}
+            />
+        })}
+         
+        </div>
+      );
+
+      style.backgroundColor = 'red';
+    }
+
+    //variable classes is just a String joined with an empty space ('red bold')
+    //---->>let classes = ['red', 'bold'].join(' ');
+
+
+    // ------
+    // to use in paragraph in return()
+    //red and bold are the classes defined in .css
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); //classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); //classes = ['red', 'bold']
+    }
+
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        {/* classes is just an array, we need to pass a string: classes.join(' ')*/}
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+      </div>
+    );
+  
+  }
+}
+
+export default App;
