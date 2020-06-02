e73c07128083bd97acddeedf0e95a566907f78ef
dccfmatos <d.catarina.c.f@gmail.com>
2020-06-02 17:42:49 +0100

    change styles dinamically with css modules

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import './App.css';
 import Person from './Person/Person';
-
-const StyledButton = styled.button`
-/*inside this component we write normal CSS */
-      background-color: ${props => props.alt ? 'red' : 'green'}; /*ternary function that allows to:
-      if true, if there is the props that we defined bellow, then this background color
-      should be red, otherwise (if false) it should continue green */
-      color: white;
-      font: inherit;
-      border: 1px solid blue;
-      padding: 8px;
-      cursor: pointer;
-
-      &:hover {
-        background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-        color: ${props => props.alt ? 'blue' : 'black'};;
-      }
-    `;
+import styles from './App.module.css';
+//React V2.0 +, just have to import this "styles" and name de css file
+//[name].module.css
+//then call styles.[class name on css] to use it
 
 class App extends Component {
   state = {
@@ -66,21 +51,9 @@ class App extends Component {
   
 
   render () {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
-
+    
     let persons = null;
+    let btnClass = [styles.Button];
 
     if ( this.state.showPersons ) {
       persons = (
@@ -98,36 +71,33 @@ class App extends Component {
         </div>
       );
 
-      /*  style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }; */
+      //change styles dinamically
+      btnClass.push(styles.Red);//btnClass will be an array with the btnClass and the Red class
     }
 
-    //variable classes is just a String joined with an empty space ('red bold')
-    //---->>let classes = ['red', 'bold'].join(' ');
+    //variable assignedClasses is just a String joined with an empty space ('red bold')
+    //---->>let assignedClasses = ['red', 'bold'].join(' ');
 
 
     // ------
     // to use in paragraph in return()
     //red and bold are the classes defined in .css
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); //classes = ['red']
+      assignedClasses.push(styles.red); //classes = ['red']
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); //classes = ['red', 'bold']
+      assignedClasses.push(styles.bold); //classes = ['red', 'bold']
     }
 
 
     return (
-      <div className="App">
+      <div className={styles.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        {/* classes is just an array, we need to pass a string: classes.join(' ')*/}
-        <StyledButton alt={this.state.showPersons} //props added so that it can be called after this method
-          onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        {/* assignedClasses is just an array, we need to pass a string: classes.join(' ')*/}
+        <button className={btnClass.join(' ')} //join all the classes in ther (.button)
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>;
         {persons}
       </div>
     );
