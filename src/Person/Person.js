5d4c95ccf79c4767c02a18e0dd05022be3a64e74
dccfmatos <d.catarina.c.f@gmail.com>
2020-06-02 10:49:02 +0100

    L69

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,8 +6,14 @@ import Radium from 'radium';
 // same as funtion() {}
 
 const person = (props) => {
+    const myStyle = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    };
+            {/*need o import {StyleRoot} */}
     return (
-        <div className="Person">
+        <div className="Person" style= {myStyle}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
